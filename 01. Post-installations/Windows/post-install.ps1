62358bbef1333317d56bcfe70edeeabd8a896cee
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-04-21 21:59:04 +0200

    Add files via upload

---
+++ 01. Post-installations/Windows/post-install.ps1
@@ -0,0 +1,23 @@
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $Command = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+        Start-Process -FilePath PowerShell.exe -Verb RunAs -ArgumentList $Command
+        Exit
+ }
+}
+
+
+
+winget install Microsoft.WindowsTerminal
+winget install Microsoft.PowerShell
+winget install Mozilla.Firefox
+winget install TheDocumentFoundation.LibreOffice
+winget install VideoLAN.VLC
+winget install 7zip.7zip
+
+#regedit /s .\favourites.reg
+reg import .\favourites.reg
+reg import .\show_file_extensions.reg
+
+#Set-ExecutionPolicy Restricted
