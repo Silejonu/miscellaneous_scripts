e6fe9e2dff11513e12bf4402555e9209d501deb2
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-04-21 19:51:15 +0200

    Delete remove_lint.sh

--- remove_lint.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-find . \( -iname '._*' -o -iname '.fseventsd' -o -iname '.Spotlight-V100' -o -iname '.TemporaryItems' -o -iname '.Trash*' -o -iname '.DS_Store' -o -iname 'Thumbs.db' -o -iname 'System Volume Information' \) -exec rm -rf {} \;
