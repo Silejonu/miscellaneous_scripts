f40230b3cca6fce1b1ba185bf8d7c2fa4a19961a
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-04-23 13:52:14 +0200

    Add files via upload

---
+++ 02. Linux/ansi_escape_sequences.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+reset_sgr='\033[0m'
+# Effects
+bold='\033[1m'
+faint='\033[2m'
+underline='\033[4m'
+blink='\033[5m'
+# Foreground colours
+black='\033[30m'
+red='\033[31m'
+green='\033[32m'
+yellow='\033[33m'
+blue='\033[34m'
+magenta='\033[35m'
+cyan='\033[36m'
+white='\033[37m'
+# Background colours
+black_bg='\033[40m'
+red_bg='\033[41m'
+green_bg='\033[42m'
+yellow_bg='\033[43m'
+blue_bg='\033[44m'
+magenta_bg='\033[45m'
+cyan_bg='\033[46m'
+white_bg='\033[47m'
+## Additional, non-standard colours
+# Foreground colours
+bright_black='\033[90m'
+bright_red='\033[91m'
+bright_green='\033[92m'
+bright_yellow='\033[93m'
+bright_blue='\033[94m'
+bright_magenta='\033[95m'
+bright_cyan='\033[96m'
+bright_white='\033[97m'
+# Background colours
+bright_black_bg='\033[100m'
+bright_red_bg='\033[101m'
+bright_green_bg='\033[102m'
+bright_yellow_bg='\033[103m'
+bright_blue_bg='\033[104m'
+bright_magenta_bg='\033[105m'
+bright_cyan_bg='\033[106m'
+bright_white_bg='\033[107m'
