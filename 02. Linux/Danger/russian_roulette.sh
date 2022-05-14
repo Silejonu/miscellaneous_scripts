8044bcaa560585c18d2b3bd46285820e6e0f55d1
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-05-14 23:59:44 +0200

    Update russian_roulette.sh

--- 02. Linux/Danger/russian_roulette.sh
+++ 02. Linux/Danger/russian_roulette.sh
@@ -2,8 +2,8 @@
 
 while true ; do
   target=$(ps -ef | tail -n +2 | shuf -n1)
-  sudo kill "$(echo "${target}" | awk '{print $2}')"
-  echo "$(echo "${target}" | awk '{print $8}') has been shot dead."
+  sudo kill "$(echo ${target} | awk '{print $2}')"
+  echo "$(echo ${target} | awk '{print $8}') has been shot dead."
   read -rp 'Dare to pull the trigger one more time? [Y/n] ' yesno
   if [[ -n ${yesno} ]] && [[ ${yesno} != [yY] ]] ; then
     echo "A bit of a coward, I see…"
