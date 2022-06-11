3d68884984e23524982d3ee33fbb6c3793e8125a
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-06-11 20:05:57 +0200

    Update dvd_rip.sh

--- 02. Linux/dvd_rip.sh
+++ 02. Linux/dvd_rip.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 read -rp 'Titre du film ? ' titre_du_film
-repertoire_encodage="~/Videos/rips/${titre_du_film}"
+repertoire_encodage="$HOME/Videos/rips/${titre_du_film}"
 
 
 mkdir -p "${repertoire_encodage}"
@@ -9,7 +9,9 @@ mkdir -p "${repertoire_encodage}"
 # Extraire les sous-titres
 cp /run/media/${USER}/*/VIDEO_TS/*.IFO "${repertoire_encodage}"
 # Extraire les méta-données
-mpv dvd://longest > "${repertoire_encodage}/meta.txt"
+mpv dvd://longest > "${repertoire_encodage}/meta.txt" &
+sleep 10
+kill $(pidof mpv)
 # Extraire le fichier .vob
 mpv dvd://longest --stream-dump="${repertoire_encodage}/${titre_du_film}.vob"
 
