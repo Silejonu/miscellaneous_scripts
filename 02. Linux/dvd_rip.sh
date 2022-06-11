1cbac20baaa362295a9fe541eb89e6ee58354e39
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-06-11 13:15:02 +0200

    Create dvd_rip.sh

---
+++ 02. Linux/dvd_rip.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+read -rp 'Titre du film ? ' titre_du_film
+repertoire_encodage="~/Videos/rips/${titre_du_film}"
+
+
+mkdir -p "${repertoire_encodage}"
+
+# Extraire les sous-titres
+cp /run/media/${USER}/*/VIDEO_TS/*.IFO "${repertoire_encodage}"
+# Extraire les méta-données
+mpv dvd://longest > "${repertoire_encodage}/meta.txt"
+# Extraire le fichier .vob
+mpv dvd://longest --stream-dump="${repertoire_encodage}/${titre_du_film}.vob"
+
